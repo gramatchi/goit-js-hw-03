5dcabd784b8cd30948587552b29d1715ad61decf
gramatchi <nik98.champion@gmail.com>
2024-06-16 14:38:00 +0200

    Do task-3

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function filterArray(numbers, value) {
+  let newArr = [];
+  for (const i of numbers) {
+    if (i > value) {
+      newArr.push(i);
+    }
+  }
+  return newArr;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
